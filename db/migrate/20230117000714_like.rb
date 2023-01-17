8a51a47633f65217822fd66965cf3c624e2e108d
cvalencia1991 <cesar4a6z@gmail.com>
2023-01-16 22:43:15 -0500

    Add schema Rails

--- db/migrate/20230117000714_like.rb
+++ db/migrate/20230117000714_like.rb
@@ -2,11 +2,8 @@ class Like < ActiveRecord::Migration[7.0]
   def change
     create_table :Like do |t|
       t.integer :Authoid ,:Postid
-      t.datetime :created_at, :updated_at
       t.timestamps
-      add_index :Authoid, :Postid
-      add_reference :Like, :User, foreign_key: true
     end
-
+    add_index :Authoid, :Postid
   end
 end
--- db/migrate/20230117001512_post.rb
+++ db/migrate/20230117001512_post.rb
@@ -4,12 +4,7 @@ class Post < ActiveRecord::Migration[7.0]
       t.integer :Authorid,:CommentsCounter,:LikesCounter
       t.string :title
       t.text :text
-      t.datetime :created_at, :updated_at
-      add_index :AuthorId, :created_at,:updated_at
-      add_reference :Post, :comment, foreign_key: true
-      add_reference :Post, :Like, foreign_key: true
       t.timestamps
     end
-
   end
 end
