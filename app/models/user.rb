4187564d127d493374d16a9ac5f3e15aa01cabca
cvalencia1991 <cesar4a6z@gmail.com>
2023-01-22 08:13:57 -0500

    Add change user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :likes, class_name: 'Like'
   has_many :comments, class_name: 'Comment'
-  has_many :posts, class_name: 'Post'
+  has_many :posts, class_name: 'Posts'
   validates :name, presence: true
   validates :postscounter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
