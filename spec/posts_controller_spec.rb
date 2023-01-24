dd1605cb5a92c3f1fb6b895e8f3350381f37eee3
cvalencia1991 <cesar4a6z@gmail.com>
2023-01-23 20:41:38 -0500

    Add  specs of Responses

--- spec/posts_controller_spec.rb
+++ spec/posts_controller_spec.rb
@@ -1,14 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe PostsController, type: :controller do
-  describe 'GET #index' do
+  describe 'specs Controller that' do
     it 'returns http success' do
       get :index, params: { user_id: 1 }
       expect(response).to have_http_status(:success)
+      expect(response.status).to eq(200)
     end
     it 'renders the index template' do
       get :index, params: { user_id: 1 }
       expect(response).to render_template(:index)
+    end
+    it 'renders the show template' do
+      get :index, params: { id: 1, user_id: 1 }
+      expect(response).to render_template(:index)
+    end
+    it 'returns http index success' do
+      get :index, params: { id: 1, user_id: 1 }
+      expect(response).to have_http_status(:success)
+    end
+    it "returns the correct placeholder text" do
+      get :show,params: { id: 1, user_id: 1 }
+      expect(assigns(:posts)).to eq("Here is a list of posts for a given user")
     end
   end
 end
--- spec/users_controller_spec.rb
+++ spec/users_controller_spec.rb
@@ -2,12 +2,21 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
   describe 'GET #index' do
-    it 'returns http success' do
+    it 'returns http index success' do
       get :index
       expect(response).to have_http_status(:success)
+      expect(response.status).to eq(200)
     end
     it 'renders the index template' do
       get :index
+      expect(response).to render_template(:index)
+    end
+    it 'returns http show success' do
+      get :index, params: { id: 1 }
+      expect(response).to have_http_status(:success)
+    end
+    it 'renders the show template' do
+      get :index, params: { id: 1 }
       expect(response).to render_template(:index)
     end
   end
