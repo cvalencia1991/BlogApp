aab1aa9c740b622814e895847d278983e847ea2b
cvalencia1991 <cesar4a6z@gmail.com>
2023-01-23 20:48:55 -0500

    Fix variable show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,6 @@ class PostsController < ApplicationController
   def index; end
 
   def show
-    @posts = "Here is a list of posts for a given user"
+    @posts = 'Here is a list of posts for a given user'
   end
 end
