4e9692a12b762411f1211966952e8cf2bb16a059
cvalencia1991 <cesar4a6z@gmail.com>
2023-01-25 17:03:10 -0500

    Fix Posts Problem

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
-  belongs_to :post, class_name: 'Post', foreign_key: 'postid'
+  belongs_to :posts, class_name: 'Post', foreign_key: 'postid'
   belongs_to :author, class_name: 'User', foreign_key: 'authorid'
   validates :postid, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   after_save :upadate_comments
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
-  has_many :likes, class_name: 'Like'
-  has_many :comments, class_name: 'Comment'
-  has_many :posts, class_name: 'Posts'
+  has_many :likes, class_name: 'Like', foreign_key: 'authorid'
+  has_many :comments, class_name: 'Comment', foreign_key: 'authorid'
+  has_many :posts, class_name: 'Post', foreign_key: 'authorid'
   validates :name, presence: true
   validates :postscounter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def recents_post
-    Post.limit(3).order(created_at: :desc)
+    posts.limit(3).order(created_at: :desc)
   end
 end
