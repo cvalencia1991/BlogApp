4e33a3ea3faddc313fe1db20e43cd663cb6517c3
cvalencia1991 <cesar4a6z@gmail.com>
2023-01-26 15:27:04 -0500

    Add Post Form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,23 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new(id: current_user.id)
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to user_path(current_user), notice: 'Post was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text).merge(authorid: current_user.id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   resources :users, only: [:index,:show] do
     resources :posts, only: [:index,:show]
   end
+  resources :posts, only:[:new,:create]
+  resources :comments, only: [:new, :create]
 end
