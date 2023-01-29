c222fb7877614839efddbddd75ad8f3387af210e
cvalencia1991 <cesar4a6z@gmail.com>
2023-01-29 11:50:35 -0500

    Fix Rubocop Files

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,13 +6,13 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
     if @comment.save
-      redirect_to user_post_path(id:current_user.id ), notice: 'Comment was successfully created.'
+      redirect_to user_post_path(id: current_user.id), notice: 'Comment was successfully created.'
     else
       render :new
     end
   end
 
   def comment_params
-    params.require(:comment).permit(:text).merge(post_id: params[:post_id],author_id: current_user.id)
+    params.require(:comment).permit(:text).merge(post_id: params[:post_id], author_id: current_user.id)
   end
 end
