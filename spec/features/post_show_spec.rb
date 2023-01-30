5e41e59c575afd12a86873d09835b2dc45068ff5
cvalencia1991 <cesar4a6z@gmail.com>
2023-01-30 18:14:48 -0500

    Solve Commiy

--- spec/features/post_show_spec.rb
+++ spec/features/post_show_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 RSpec.describe Post, type: :system do
-  describe 'post show user page' do
+  describe 'show user page' do
     it 'I can see the posts title.' do
       visit user_post_path(1,1)
       expect(page).to have_content('post 1')
@@ -36,10 +36,10 @@ RSpec.describe Post, type: :system do
 
     it 'I can see the comment each commentor left.' do
       visit user_post_path(1,1)
-      post = Post.includes(:comments,comments:[:author]).first
-      post.comments.each do |comment|
-        expect(page).to have_content(`#{comment.author.name}:#{comment.text}`)
-      end
+      post = Post.includes(:comments,comments:[:author]).last
+      comment = post.comments.first
+      comment_post  = comment.author.name.concat(':').concat(comment.text)
+      expect(page).to have_content(comment_post)
     end
 
 
