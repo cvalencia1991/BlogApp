0acb5f95987be6280de6a42cd3eaaabe7ee1bad4
cvalencia1991 <cesar4a6z@gmail.com>
2023-01-30 17:01:35 -0500

    Post Show Spec

---
+++ spec/features/post_show_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+RSpec.describe Post, type: :system do
+  describe 'post show user page' do
+    it 'I can see the posts title.' do
+      visit user_post_path(1,1)
+      expect(page).to have_content('post 1')
+    end
+
+    it 'I can see the posts title.' do
+      visit user_post_path(1,1)
+      expect(page).to have_content('Tom')
+    end
+
+    it 'I can see the posts title.' do
+      visit user_post_path(1,1)
+      expect(page).to have_content('Comments: 9')
+    end
+
+    it 'I can see how many likes it has.' do
+      visit user_post_path(1,1)
+      expect(page).to have_content('Likes: 9')
+    end
+
+    it 'I can see the post body.' do
+      visit user_post_path(1,1)
+      expect(page).to have_content('post 1')
+    end
+
+    it 'I can see the username of each commentor.' do
+      visit user_post_path(1,1)
+      post = Post.includes(:comments,comments:[:author]).first
+      post.comments.each do |comment|
+        expect(page).to have_content(comment.author.name)
+      end
+    end
+
+    it 'I can see the comment each commentor left.' do
+      visit user_post_path(1,1)
+      post = Post.includes(:comments,comments:[:author]).first
+      post.comments.each do |comment|
+        expect(page).to have_content(`#{comment.author.name}:#{comment.text}`)
+      end
+    end
+
+
+
+  end
+end
