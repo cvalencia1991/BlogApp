3affad2f8ea716f3bc34847a29b8caa5b2d5dbd0
cvalencia1991 <cesar4a6z@gmail.com>
2023-01-31 17:14:52 -0500

    Add User view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   protect_from_forgery with: :exception
-
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :update_allowed_parameters, if: :devise_controller?
 
   protected
@@ -10,4 +10,11 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name,:bio,:photo ,:email, :password)}
     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email,:bio,:photo, :password, :current_password)}
   end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_in) do |user_params|
+      user_params.permit(:username, :email)
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'home#index'
+  root to:  'users#index'
   resources :users, only: [:index,:show] do
     resources :posts, only: [:index,:show] do
       resources :likes, only:[:create,:show]
       resources :comments, only:[:new,:create]
     end
   end
+  delete "logout", to: "sessions#destroy"
   resources :posts, only:[:new,:create]
 end
